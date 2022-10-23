f76997de6d9db3d40411a11ffc2427cc69387898
pagAndr <90697273+pagAndr@users.noreply.github.com>
2022-10-23 16:55:08 +0300

    fwefdw

--- FCIMlendar/backend/fcimlendar/accounts/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from accounts import models
-
-
-class AccountsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Account
-        exclude = ['is_admin']
- 
--- FCIMlendar/backend/fcimlendar/schedule/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from schedule import views
-
-urlpatterns = [
-    path('', views.ScheduleView.as_view()),
-]
